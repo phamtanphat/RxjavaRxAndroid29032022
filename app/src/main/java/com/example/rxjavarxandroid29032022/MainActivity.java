cce414fcdad0e920d8309b1616918c203902afe8
Phat <phatdroid94@gmail.com>
2022-06-11 18:45:27 +0700

    random color

--- app/src/main/java/com/example/rxjavarxandroid29032022/MainActivity.java
+++ app/src/main/java/com/example/rxjavarxandroid29032022/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.rxjavarxandroid29032022;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.Random;
+
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.functions.Function;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText edtInput;
+    Button btnAssignColor;
+    TextView tvOutput;
+    Observable<String> stringObservable;
+    int color;
+    Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtInput = findViewById(R.id.edit_text);
+        btnAssignColor = findViewById(R.id.button_assign_color);
+        tvOutput = findViewById(R.id.text_view_output);
+
+        random = new Random();
+
+        btnAssignColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = edtInput.getText().toString();
+
+                if (!input.isEmpty()) {
+                    stringObservable = Observable.just(input);
+
+                    stringObservable
+                            .subscribeOn(Schedulers.io())
+                            .map(new Function<String, Spanned>() {
+                                @Override
+                                public Spanned apply(String s) throws Throwable {
+                                    color = Color.rgb(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                                        return Html.fromHtml("<font color=" + color + ">" + s + "</font>", Html.FROM_HTML_MODE_LEGACY);
+                                    } else {
+                                        return Html.fromHtml("<font color=" + color + ">" + s + "</font>");
+                                    }
+                                }
+                            })
+                }
+            }
+        });
     }
 }
